031eef7732023d0f13be9858aa9d948a6905e38a
Nejc Anclin <nejc.anclin@gmail.com>
2017-01-20 22:34:21 +0100

    Character jumps.

--- Assets/Code/CharacterController.cs
+++ Assets/Code/CharacterController.cs
@@ -3,7 +3,13 @@
 public class CharacterController : MonoBehaviour {
 
     [SerializeField] private float WalkSpeed = 1;
+    [SerializeField] private float JumpForce = 100;
+    [SerializeField] private Transform TopLeft;
+    [SerializeField] private Transform BottomRight;
     [SerializeField] private Rigidbody2D Rigidbody2D;
+    [SerializeField] private LayerMask GroundLayerMask;
+
+    bool IsOnGround;
 
     void Update() {
 
@@ -12,6 +18,13 @@ public class CharacterController : MonoBehaviour {
 
         if (Mathf.Abs(xAxis) > 0) {
             Debug.Log(string.Format("xAxis={0}", xAxis));
+        }
+
+        IsOnGround = Physics2D.OverlapArea(TopLeft.position, BottomRight.position, GroundLayerMask);
+        Debug.Log(string.Format("IsOnGround={0}", IsOnGround));
+
+        if (IsOnGround && Input.GetButtonDown("Fire2")) {
+            Rigidbody2D.AddForce(Vector3.up * JumpForce);
         }
 
         float xStep = xAxis * WalkSpeed;
