87441c143d542df5d80899088eb357bd8ef4f446
Nejc Anclin <nejc.anclin@gmail.com>
2017-01-20 22:06:08 +0100

    CharacterController implemented, move left, right, reset when position on drop.

---
+++ Assets/Code/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    [SerializeField] private float WalkSpeed = 1;
+    [SerializeField] private Rigidbody2D Rigidbody2D;
+
+    void Update() {
+
+        // Get input
+        float xAxis = Input.GetAxis("Horizontal");
+
+        if (Mathf.Abs(xAxis) > 0) {
+            Debug.Log(string.Format("xAxis={0}", xAxis));
+        }
+
+        float xStep = xAxis * WalkSpeed;
+        float yStep = 0;
+
+        // Update position
+        transform.position += new Vector3(xStep, yStep);
+
+        // Reset if fallen down
+        if (transform.position.y < -3) {
+            transform.position = Vector3.zero;
+            Rigidbody2D.velocity = Vector2.zero;
+        }
+    }
+}
