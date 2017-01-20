3a89f3e71e4cf61e74e240fb21fcea315dd7fe22
Nejc Anclin <nejc.anclin@gmail.com>
2017-01-20 23:45:40 +0100

    Second character added.

--- Assets/Code/CharacterController.cs
+++ Assets/Code/CharacterController.cs
@@ -8,22 +8,35 @@ public class CharacterController : MonoBehaviour {
     [SerializeField] private Transform BottomRight;
     [SerializeField] private Rigidbody2D Rigidbody2D;
     [SerializeField] private LayerMask GroundLayerMask;
+    [SerializeField] private int PlayerNumber;
 
-    bool IsOnGround;
+    private bool IsOnGround;
+    private Vector3 Speed;
+    private Vector3 Acceleration;
 
     void Update() {
 
         // Get input
-        float xAxis = Input.GetAxis("Horizontal");
+        float xAxis1 = Input.GetAxis("Horizontal1");
+        float xAxis2 = Input.GetAxis("Horizontal2");
+        bool jump1 = Input.GetButtonDown("JumpP1");
+        bool jump2 = Input.GetButtonDown("JumpP2");
 
-        if (Mathf.Abs(xAxis) > 0) {
-            Debug.Log(string.Format("xAxis={0}", xAxis));
+        MoveUpdate(PlayerNumber == 1 ? xAxis1 : xAxis2, PlayerNumber == 1 ? jump1 : jump2);
+
+        // Reset if fallen down
+        if (transform.position.y < -3) {
+            transform.position = Vector3.zero;
+            Rigidbody2D.velocity = Vector2.zero;
         }
+    }
+
+    private void MoveUpdate(float xAxis, bool jump) {
 
         IsOnGround = Physics2D.OverlapArea(TopLeft.position, BottomRight.position, GroundLayerMask);
-        Debug.Log(string.Format("IsOnGround={0}", IsOnGround));
+        //        Debug.Log(string.Format("IsOnGround={0}", IsOnGround));
 
-        if (IsOnGround && Input.GetButtonDown("Fire2")) {
+        if (IsOnGround && jump) {
             Rigidbody2D.AddForce(Vector3.up * JumpForce);
         }
 
@@ -33,10 +46,5 @@ public class CharacterController : MonoBehaviour {
         // Update position
         transform.position += new Vector3(xStep, yStep);
 
-        // Reset if fallen down
-        if (transform.position.y < -3) {
-            transform.position = Vector3.zero;
-            Rigidbody2D.velocity = Vector2.zero;
-        }
     }
 }
