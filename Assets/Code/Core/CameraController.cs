06f71de4fe9c1c86f7ed5f71878a2838cc38b091
Nejc Anclin <nejc.anclin@gmail.com>
2017-01-22 00:43:37 +0100

    Camera follow.

---
+++ Assets/Code/Core/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField] private CharacterController CharacterController1;
+    [SerializeField] private CharacterController CharacterController2;
+    [SerializeField] private float AttractSpeed = 10;
+
+    void Update() {
+        
+        Vector3 charactersCenter = (CharacterController1.transform.position + CharacterController2.transform.position) * 0.5f;
+        Vector3 dif = charactersCenter - transform.position;
+        float dis = Vector3.Magnitude(dif);
+        Vector3 moveVector = dif.normalized * dis * AttractSpeed;
+        moveVector.y = 0;
+
+        transform.Translate(moveVector);
+    }
+}
