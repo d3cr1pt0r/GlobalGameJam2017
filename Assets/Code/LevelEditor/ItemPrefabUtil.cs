336f514c6b772d47a9838289fe7370a9433579aa
Klemen Brajkovic <klemen.brajkovic@outfit7.com>
2017-01-21 23:20:59 +0100

    pattern editing done

---
+++ Assets/Code/LevelEditor/ItemPrefabUtil.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+
+
+public class ItemPrefabUtil
+{
+	private const string Tag = "ItemPrefabUtil";
+	public static string ItemPrefabsPath = "Assets/Resources/Prefabs/Items";
+
+	public static List<GameObject> GetAllGoalPrefabs ()
+	{
+		List<GameObject> prefabs = new List<GameObject> ();
+		string[] items = Directory.GetFiles (ItemPrefabUtil.ItemPrefabsPath);
+
+		for (int i = 0; i < items.Length; i++) {
+			string path = items [i];
+			string name = Path.GetFileNameWithoutExtension (path);
+			if (name.StartsWith ("goal_") && !name.Contains ("goal_proxy") && path.EndsWith (".prefab")) {
+				GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject> (path) as GameObject;
+
+				if (prefab != null) {
+					prefabs.Add (prefab);
+				} else {
+					Log.LogDebug (Tag, "GetAllGoalPrefabs failed to LoadAssetAtPath {0}", path);
+				}
+			}
+		}
+
+		return prefabs;
+	}
+
+	public static List<GameObject> GetAllDebreePrefabs ()
+	{
+		List<GameObject> prefabs = new List<GameObject> ();
+		string[] items = Directory.GetFiles (ItemPrefabUtil.ItemPrefabsPath);
+
+		for (int i = 0; i < items.Length; i++) {
+			string path = items [i];
+			string name = Path.GetFileNameWithoutExtension (path);
+			if (name.StartsWith ("debree_") && !name.Contains ("debree_proxy") && path.EndsWith (".prefab")) {
+				GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject> (path) as GameObject;
+
+				if (prefab != null) {
+					prefabs.Add (prefab);
+				} else {
+					Log.LogDebug (Tag, "GetAllGoalPrefabs failed to LoadAssetAtPath {0}", path);
+				}
+			}
+		}
+
+		return prefabs;
+	}
+
+	public static GameObject GetDebreeProxy ()
+	{
+		string path = ItemPrefabsPath + "/debree_proxy.prefab";
+
+		if (!File.Exists (path)) {
+			Log.LogDebug (Tag, "GetDebreeProxy file does not exist {0}", path);
+			return null;
+		}
+
+		return AssetDatabase.LoadAssetAtPath<GameObject> (path) as GameObject;
+	}
+
+	public static GameObject GetGoalProxy ()
+	{
+		string path = ItemPrefabsPath + "/goal_proxy.prefab";
+
+		if (!File.Exists (path)) {
+			Log.LogDebug (Tag, "GetDebreeProxy file does not exist {0}", path);
+			return null;
+		}
+
+		return AssetDatabase.LoadAssetAtPath<GameObject> (path) as GameObject;
+	}
+
+}
--- Assets/Code/LevelEditor/LevelEditorGUI.cs
+++ Assets/Code/LevelEditor/LevelEditorGUI.cs
@@ -9,26 +9,20 @@ public class LevelEditorGUI : Editor
 {
 
 	private const string Tag = "LevelEditorGUI";
-	private const string ItemPrefabsPath = "Assets/Resources/Prefabs/Items";
 
-	private List<GameObject> AddedItems;
 	private string patternName = "pattern_1";
+
+	private LevelEditor LevelEditor;
 
 	public override void OnInspectorGUI ()
 	{
 		DrawDefaultInspector ();
 
-		LevelEditor LevelEditor = (LevelEditor)target;
+		LevelEditor = (LevelEditor)target;
 
 		GUILayout.BeginHorizontal ();
 		if (GUILayout.Button ("Load Level")) {
-			if (LevelEditor.LevelPrefab == null) {
-				Log.LogDebug (Tag, "Load Level failed (no level loaded)");
-				return;
-			}
-
-			GameObject levelPrefab = Instantiate (LevelEditor.LevelPrefab, Vector3.zero, Quaternion.identity);
-			LevelEditor.LoadedLevel = levelPrefab;
+			LoadLevel ();
 		}
 
 		if (GUILayout.Button ("Save Level")) {
@@ -36,10 +30,19 @@ public class LevelEditorGUI : Editor
 		}
 		GUILayout.EndHorizontal ();
 
+		GUILayout.BeginHorizontal ();
+		if (GUILayout.Button ("Load Pattern")) {
+			LoadPattern ();
+		}
+
+		if (GUILayout.Button ("Save Pattern")) {
+			PrefabUtility.ReplacePrefab (LevelEditor.LoadedLevel, LevelEditor.LevelPrefab, ReplacePrefabOptions.ReplaceNameBased);
+		}
+		GUILayout.EndHorizontal ();
+
 		GUILayout.Label ("Level Items:");
 
-		string[] items = Directory.GetFiles (ItemPrefabsPath);
-		AddedItems = new List<GameObject> ();
+		string[] items = Directory.GetFiles (ItemPrefabUtil.ItemPrefabsPath);
 
 		for (int i = 0; i < items.Length; i++) {
 			string path = items [i];
@@ -50,16 +53,7 @@ public class LevelEditorGUI : Editor
 			}
 
 			if (GUILayout.Button (name)) {
-				GameObject itemObject = AssetDatabase.LoadAssetAtPath<GameObject> (path);
-				GameObject itemPrefab = Instantiate (itemObject, Vector3.zero, Quaternion.identity);
-
-				GameObject container = GameObject.Find ("PatternContainer");
-				if (container == null) {
-					container = new GameObject ("PatternContainer");
-				}
-
-				itemPrefab.transform.SetParent (container.transform);
-
+				LoadItemPrefab (path);
 			}
 		}
 
@@ -67,64 +61,132 @@ public class LevelEditorGUI : Editor
 		patternName = GUILayout.TextField (patternName, 25);
 
 		if (GUILayout.Button ("Save Pattern")) {
-			GameObject container = GameObject.Find ("PatternContainer");
-			if (container == null) {
-				Log.LogDebug (Tag, "No PatternContainer GameObject found");
-				return;
+			SavePattern ();
+		}
+	}
+
+	private void LoadLevel ()
+	{
+		if (LevelEditor.LevelPrefab == null) {
+			Log.LogDebug (Tag, "Load Level failed (no level loaded)");
+			return;
+		}
+
+		GameObject levelPrefab = Instantiate (LevelEditor.LevelPrefab, Vector3.zero, Quaternion.identity);
+		LevelEditor.LoadedLevel = levelPrefab;
+	}
+
+	private void LoadPattern ()
+	{
+		if (LevelEditor.Pattern == null) {
+			Log.LogDebug (Tag, "Load Pattern failed (no pattern loaded)");
+			return;
+		}
+
+		if (LevelEditor.LoadedLevel == null) {
+			LoadLevel ();
+		}
+
+		GameObject container = GameObject.Find ("PatternContainer");
+		if (container != null) {
+			Log.LogDebug (Tag, "PatternContainer found! Delete currently loaded pattern data and try again!");
+			return;
+		} else {
+			container = new GameObject ("PatternContainer");
+		}
+
+		for (int i = 0; i < LevelEditor.Pattern.ItemNodes.Count; i++) {
+			Pattern.ItemNode itemNode = LevelEditor.Pattern.ItemNodes [i];
+			GameObject itemPrefab = null;
+
+			if (itemNode.ItemPrefab == null) {
+				if (itemNode.ItemType == Enums.ItemType.GOAL) {
+					itemPrefab = ItemPrefabUtil.GetGoalProxy ();
+				}
+				if (itemNode.ItemType == Enums.ItemType.DEBREE) {
+					itemPrefab = ItemPrefabUtil.GetGoalProxy ();
+				}
+			} else {
+				itemPrefab = itemNode.ItemPrefab;
 			}
 
-			Pattern pattern = ScriptableObject.CreateInstance<Pattern> ();
-			pattern.ItemNodes = new List<Pattern.ItemNode> ();
-			pattern.RootPosition = container.transform.position;
+			itemPrefab = Instantiate (itemPrefab, itemNode.Position, Quaternion.identity);
+			itemPrefab.transform.SetParent (container.transform);
+		}
+	}
 
-			for (int i = 0; i < container.transform.childCount; i++) {
-				GameObject patternItem = container.transform.GetChild (i).gameObject;
-				Pattern.ItemNode itemNode = new Pattern.ItemNode ();
+	private void SavePattern ()
+	{
+		GameObject container = GameObject.Find ("PatternContainer");
+		if (container == null) {
+			Log.LogDebug (Tag, "No PatternContainer GameObject found");
+			return;
+		}
 
-				if (patternItem.name.Contains ("goal_proxy")) {
-					itemNode.ItemPrefab = null;
-					itemNode.ItemType = Enums.ItemType.GOAL;
-					itemNode.Position = patternItem.transform.position;
-				} else if (patternItem.name.Contains ("debree_proxy")) {
-					itemNode.ItemPrefab = null;
-					itemNode.ItemType = Enums.ItemType.GOAL;
-					itemNode.Position = patternItem.transform.position;
-				} else {
-					string path = null;
-					string[] files = Directory.GetFiles (ItemPrefabsPath);
-					for (int j = 0; j < files.Length; j++) {
-						string name = Path.GetFileNameWithoutExtension (files [j]);
-						if (patternItem.name.Contains (name)) {
-							path = files [j];
-							break;
-						}
+		Pattern pattern = ScriptableObject.CreateInstance<Pattern> ();
+		pattern.ItemNodes = new List<Pattern.ItemNode> ();
+		pattern.RootPosition = container.transform.position;
+
+		for (int i = 0; i < container.transform.childCount; i++) {
+			GameObject patternItem = container.transform.GetChild (i).gameObject;
+			Pattern.ItemNode itemNode = new Pattern.ItemNode ();
+
+			if (patternItem.name.Contains ("goal_proxy")) {
+				itemNode.ItemPrefab = null;
+				itemNode.ItemType = Enums.ItemType.GOAL;
+				itemNode.Position = patternItem.transform.position;
+			} else if (patternItem.name.Contains ("debree_proxy")) {
+				itemNode.ItemPrefab = null;
+				itemNode.ItemType = Enums.ItemType.GOAL;
+				itemNode.Position = patternItem.transform.position;
+			} else {
+				string path = null;
+				string[] files = Directory.GetFiles (ItemPrefabUtil.ItemPrefabsPath);
+				for (int j = 0; j < files.Length; j++) {
+					string name = Path.GetFileNameWithoutExtension (files [j]);
+					if (patternItem.name.Contains (name)) {
+						path = files [j];
+						break;
 					}
-
-					if (path == null) {
-						Log.LogDebug (Tag, "Could not find prefab for spawned object {0}", patternItem.name);
-						return;
-					}
-
-					GameObject originalItemPrefab = AssetDatabase.LoadAssetAtPath (path, typeof(GameObject)) as GameObject;
-
-					Debug.Log (path);
-					Debug.Log (originalItemPrefab);
-
-					itemNode.ItemPrefab = originalItemPrefab;
-					itemNode.ItemType = patternItem.GetComponent<ItemController> ().GetItemType ();
-					itemNode.Position = patternItem.transform.position;
 				}
 
-				pattern.ItemNodes.Add (itemNode);
+				if (path == null) {
+					Log.LogDebug (Tag, "Could not find prefab for spawned object {0}", patternItem.name);
+					return;
+				}
+
+				GameObject originalItemPrefab = AssetDatabase.LoadAssetAtPath (path, typeof(GameObject)) as GameObject;
+
+				Debug.Log (path);
+				Debug.Log (originalItemPrefab);
+
+				itemNode.ItemPrefab = originalItemPrefab;
+				itemNode.ItemType = patternItem.GetComponent<ItemController> ().GetItemType ();
+				itemNode.Position = patternItem.transform.position;
 			}
 
-			AssetDatabase.CreateAsset (pattern, "Assets/Resources/Universe/Patterns/" + patternName + ".asset");
-			AssetDatabase.SaveAssets ();
-
-			EditorUtility.FocusProjectWindow ();
-
-			Selection.activeObject = pattern;
+			pattern.ItemNodes.Add (itemNode);
 		}
+
+		AssetDatabase.CreateAsset (pattern, "Assets/Resources/Universe/Patterns/" + patternName + ".asset");
+		AssetDatabase.SaveAssets ();
+
+		EditorUtility.FocusProjectWindow ();
+
+		Selection.activeObject = pattern;
+	}
+
+	private void LoadItemPrefab (string path)
+	{
+		GameObject itemObject = AssetDatabase.LoadAssetAtPath<GameObject> (path);
+		GameObject itemPrefab = Instantiate (itemObject, Vector3.zero, Quaternion.identity);
+
+		GameObject container = GameObject.Find ("PatternContainer");
+		if (container == null) {
+			container = new GameObject ("PatternContainer");
+		}
+
+		itemPrefab.transform.SetParent (container.transform);
 	}
 
 }
